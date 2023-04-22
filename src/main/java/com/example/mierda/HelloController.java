54e5bf4c4e72b8943514cae426b28a149406699c
tashbash59 <tashbaev300@gmail.com>
2023-04-22 18:39:00 +0300

    Добавлена реализация кнопок, анимация сна

--- src/main/java/com/example/mierda/HelloController.java
+++ src/main/java/com/example/mierda/HelloController.java
@@ -2,22 +2,46 @@ package com.example.mierda;
 
 import com.example.mierda.calendar.CalendarEntry;
 import com.example.mierda.calendar.CalendarModel;
+
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.animation.Animation;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.image.Image;
+import javafx.util.Duration;
 
 public class HelloController implements Initializable {
     @FXML public AnchorPane calendarComponent;
+    @FXML public ImageView mierdaAnimation;
+    @FXML public Button eat;
+    @FXML public AnchorPane eatBar;
+    @FXML public Button health;
+    @FXML public AnchorPane healthBar;
+    @FXML public Button happy;
+    @FXML public AnchorPane happyBar;
+    @FXML public Label moneyLabel;
+
+    static Integer money = 1000;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        moneyLabel.setText(money.toString());
+        initAnimation();
         initCalendarComponent();
+        eating(eatBar,eat);
+        eating(healthBar,health);
+        eating(happyBar,happy);
     }
 
     private void initCalendarComponent() {
@@ -46,5 +70,52 @@ public class HelloController implements Initializable {
                 this.calendarComponent.getChildren().add(label);
             }
         }
+    }
+    public void initAnimation() {
+
+
+        File animationFile = new File("C:\\Users\\Danil\\Desktop\\python\\CW\\src\\main\\images\\sleep.png");
+        Image ANIMATION_IMAGE = new Image(animationFile.toURI().toString());
+        File defaultMierda = new File("C:\\Users\\Danil\\Desktop\\python\\CW\\src\\main\\images\\mierda.png");
+        Image MIERDA = new Image(defaultMierda.toURI().toString());
+
+        final int COLUMNS  =  3;
+        final int COUNT    =  9;
+        final int OFFSET_X =  1;
+        final int OFFSET_Y =  1;
+        final int WIDTH    = 882;
+        final int HEIGHT   = 892;
+
+        mierdaAnimation.setImage(ANIMATION_IMAGE);
+        mierdaAnimation.setViewport(new Rectangle2D(OFFSET_X, OFFSET_Y, WIDTH, HEIGHT));
+        //final ImageView imageView = new ImageView(IMAGE);
+        //imageView.setViewport(new Rectangle2D(OFFSET_X, OFFSET_Y, WIDTH, HEIGHT));
+        final Animation animation = new SpriteAnimation(
+                mierdaAnimation,
+                Duration.millis(1000),
+                COUNT, COLUMNS,
+                OFFSET_X, OFFSET_Y,
+                WIDTH, HEIGHT
+        );
+        animation.setCycleCount(Animation.INDEFINITE);
+        animation.play();
+    }
+
+    private void eating(AnchorPane bar,Button button) {
+        final double onePartBar = 43.4;
+        button.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (bar.getPrefWidth() < bar.getMaxWidth() - onePartBar) {
+                    bar.setPrefWidth(bar.getWidth() + onePartBar);
+                    money -= 10;
+                    moneyLabel.setText(money.toString());
+                } else if (bar.getPrefWidth() < bar.getMaxWidth()) {
+                    bar.setPrefWidth(bar.getMaxWidth());
+                    money -= 10;
+                    moneyLabel.setText(money.toString());
+                }
+            }
+        });
     }
 }
